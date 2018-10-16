dab06f17591c059b14cfa553a952f52d51bd95c1
zzxdng <zhao-zhongxin@qq.com>
2018-10-16 20:07:11 +0800

    1.添加ViewPagerDemo。

---
+++ app/src/main/java/com/zinc/example/androidlab/viewpagerdemo/ViewPagerDemoActivity.java
@@ -0,0 +1,15 @@
+package com.zinc.example.androidlab.viewpagerdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.zinc.example.androidlab.R;
+
+public class ViewPagerDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_pager_demo);
+    }
+}
