34659d9d4635e544a0e4748c809c27a7a7d01181
zzxdng <zhao-zhongxin@qq.com>
2018-10-20 18:28:34 +0800

    1.添加SVGA播放Demo文件。

---
+++ app/src/main/java/com/zinc/example/androidlab/svga_play_demo/SVGAPlayDemoActivity.java
@@ -0,0 +1,15 @@
+package com.zinc.example.androidlab.svga_play_demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.zinc.example.androidlab.R;
+
+public class SVGAPlayDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_svga_play_demo);
+    }
+}
